a360d934121e710e858035dc384072d23625cbef
beatrice <bbrownmulry@gmail.com>
2026-06-08 12:16:42 -0400

    add initial exam object

---
+++ embed_toolkit/structure/clinical/exams.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from datetime import date
+
+
+class BreastDensity(Enum):
+    A = 1.0
+    B = 2.0
+    C = 3.0
+    D = 4.0
+    MALE = 5.0
+
+class Exam:
+    def __init__(self):
+        self.acc_anon: int
+        self.study_date_anon: date
+        self.desc: str # study description
+
+        self.tissue_density: BreastDensity
+        self.patient_age: float
+        self.patient_first_3_zip: int
+
+        self.visit_type: str # vtype
+        self.site_id: str # from loc_num
+        self.tech_init: str # do we actually need this? i don't think so?
+
+        self.init: str # what actually is this...?
+        self.proccode: str # and this..? 
+        self.special_case_type: str # case
+
+        # derived, maybe just re-count findings instead
+        self.total_L_find: int
+        self.total_R_find: int
+
