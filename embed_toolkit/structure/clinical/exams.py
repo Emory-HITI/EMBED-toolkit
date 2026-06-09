68b48d74d6c89878f517cd39ec2af170c5a049cc
beatrice <bbrownmulry@gmail.com>
2026-06-09 10:32:12 -0400

    convert Exam to an abc and start splitting into screening/diagnostic/other variants

--- embed_toolkit/structure/clinical/exams.py
+++ embed_toolkit/structure/clinical/exams.py
@@ -1,18 +1,20 @@
 from enum import Enum
 from datetime import date
+from abc import ABC
 
 
 class BreastDensity(Enum):
+    # TODO: move to general concepts location
     A = 1.0
     B = 2.0
     C = 3.0
     D = 4.0
     MALE = 5.0
 
-class Exam:
+class Exam(ABC):
     def __init__(self):
         self.acc_anon: int
-        self.study_date_anon: date
+        self.date_anon: date
         self.desc: str # study description
 
         self.tissue_density: BreastDensity
@@ -30,4 +32,17 @@ class Exam:
         # derived, maybe just re-count findings instead
         self.total_L_find: int
         self.total_R_find: int
+
+
+class ScreeningExam(Exam):
+    def __repr__(self) -> str:
+        return f"ScreeningExam({self.date_anon} - {self.desc})"
+
+class DiagnosticExam(Exam):
+    def __repr__(self) -> str:
+        return f"DiagnosticExam({self.date_anon} - {self.desc})"
+
+class OtherExam(Exam):
+    def __repr__(self) -> str:
+        return f"OtherExam({self.date_anon} - {self.desc})"
 
